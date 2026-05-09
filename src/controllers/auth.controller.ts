ca9d7c58d4afcd3328587ddd0769c676bca758c4
Carlos Renato <carlos_sdossantos@hotmail.com>
2026-05-09 19:17:39 -0300

    feat: adicionada signout

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,5 @@
 import { FastifyReply, FastifyRequest } from "fastify";
-import { loginUser, registerUser } from "../services/auth.service.js";
+import { loginUser, loginWithGoogle, registerUser } from "../services/auth.service.js";
 import { AuthRequest, RegisterRequest } from "../types/index.js";
 import { loginSchema, registerSchema } from "../utils/validators.js";
 
@@ -44,4 +44,42 @@ export const login = async (request: FastifyRequest<{ Body: AuthRequest }>, repl
 export const profile = async (request: FastifyRequest, reply: FastifyReply) => {
 	reply.status(200).send(request.user);
 };
+
+export const googleLogin = async(request: FastifyRequest<{ Body: { credential: string } }>, reply: FastifyReply) => {	
+
+  const { credential } = request.body
+
+  if(!credential){
+	reply.status(400).send({ message: "Credencial do Google é obrigatória."});
+	return;
+  }
+
+  // Lógica de login com Google OAuth2
+  const user = await loginWithGoogle(credential, reply);
+
+  if(!user) return;
+
+  const token = request.server.jwt.sign({ userId: user.id});
+
+  reply.setCookie("syntaxwear.token", token, {
+	httpOnly: true,
+	secure: process.env.NODE_ENV === "production",
+	sameSite: "lax",
+	path: "/",
+	maxAge: 60 * 60 * 24,
+  });
+
+  reply.status(200).send({ user });
+	
+}
+
+export const signOut = async (request: FastifyRequest, reply: FastifyReply) => {
+	reply.clearCookie("syntaxwear.token", {
+		httpOnly: true,
+		secure: process.env.NODE_ENV === "production",
+		sameSite: "lax",
+		path: "/",
+	});
+	reply.status(200).send({ message: "Usuário deslogado com sucesso" });
+}
 
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -2,6 +2,7 @@ import { FastifyReply } from "fastify";
 import { AuthRequest, RegisterRequest } from "../types/index.js";
 import { prisma } from "../utils/prisma.js";
 import bcrypt from "bcrypt";
+import { OAuth2Client } from "google-auth-library";
 
 export const registerUser = async (payload: RegisterRequest) => {
 
@@ -64,5 +65,44 @@ export const loginUser = async (data: AuthRequest, reply: FastifyReply) => {
 
     return userWithoutPassword;
 };
+
+const googleClient = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
+export const loginWithGoogle = async (credential: string, reply: FastifyReply) => {
+	const ticket = await googleClient.verifyIdToken({
+		idToken: credential,
+		audience: process.env.GOOGLE_CLIENT_ID,
+	});
+
+	const payload = ticket.getPayload();
+
+	if(!payload || !payload.email){
+		// 401 Unauthorized
+		reply.status(401).send({ message: "Token do Google inválido."});
+		return;
+	}
+
+	const { email, given_name, family_name} = payload;
+
+	let user = await prisma.user.findUnique({
+		where: { email },
+	});
+
+	if(!user){
+        user = await prisma.user.create({
+            data: {
+                firstName: given_name || "",
+                lastName: family_name || "",
+                email,
+				password: "",	
+                role: "USER",
+            },
+        });
+	}
+
+	// Remover password antes de retornar
+	const { password, ...userWithoutPassword } = user;
+	return userWithoutPassword;
+}
 
 
